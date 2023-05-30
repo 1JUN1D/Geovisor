f9c716f1edfaa63f4e6187d233c04b86557b1ed7
1JUN1D <127701099+1JUN1D@users.noreply.github.com>
2023-05-30 01:25:36 -0500

    Cambios

---
+++ data/CentrosdeSalud_6.js
@@ -0,0 +1,12 @@
+var json_CentrosdeSalud_6 = {
+"type": "FeatureCollection",
+"name": "CentrosdeSalud_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1.0, "nombre": "Centro de Salud La Rivera", "tipo": "EPS", "telefono": 555675834.0, "direccion": "Cra. 1f #6437, Comuna 5, Cali, Valle del Cauca" }, "geometry": { "type": "Point", "coordinates": [ -76.490785280181569, 3.474343298290981 ] } },
+{ "type": "Feature", "properties": { "id": 2.0, "nombre": "ESE Norte", "tipo": "IPS", "telefono": 555945123.0, "direccion": "64a-73, Cra. 1g #64a-1, Cali, Valle del Cauca" }, "geometry": { "type": "Point", "coordinates": [ -76.490812341551063, 3.473705349199691 ] } },
+{ "type": "Feature", "properties": { "id": 3.0, "nombre": "Coomeva EPA", "tipo": "EPS", "telefono": 333645134.0, "direccion": "Cra. 2 #57-05, Comuna 5, Cali, Valle del Cauca" }, "geometry": { "type": "Point", "coordinates": [ -76.498241836588775, 3.4696761677065 ] } },
+{ "type": "Feature", "properties": { "id": 4.0, "nombre": "Rey DAVID", "tipo": "IPS", "telefono": 55536212.0, "direccion": "Cre. 1h #60-18" }, "geometry": { "type": "Point", "coordinates": [ -76.494950182701146, 3.472090403138296 ] } },
+{ "type": "Feature", "properties": { "id": 5.0, "nombre": "ClinicA", "tipo": "IPS", "telefono": 555581233.0, "direccion": "Calle 62B #30 Comuna 5, Cali, Valle del Cauca" }, "geometry": { "type": "Point", "coordinates": [ -76.493208157324261, 3.478195092592804 ] } }
+]
+}
---
+++ data/ReddeGas_5.js
@@ -0,0 +1,12 @@
+var json_ReddeGas_5 = {
+"type": "FeatureCollection",
+"name": "ReddeGas_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1.0, "estado": "Bueno", "material": "Acero", "diametro": 3.0, "longitud": 531.00016 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -76.502503513276324, 3.462241731336196 ], [ -76.500604405144472, 3.462823439570224 ], [ -76.501311698396535, 3.465538935020396 ] ] ] } },
+{ "type": "Feature", "properties": { "id": 2.0, "estado": "Bueno", "material": "Acero", "diametro": 6.0, "longitud": 3303.5505 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -76.501311698396535, 3.465538935020396 ], [ -76.497904125617765, 3.466604118750986 ], [ -76.498599972940681, 3.469215268857016 ], [ -76.496055133703422, 3.469973131696484 ], [ -76.497122827094941, 3.47346983661595 ], [ -76.49331794808748, 3.474628382458286 ], [ -76.49388960545113, 3.476378499859535 ], [ -76.494542521587874, 3.47743247555267 ], [ -76.494714972221999, 3.477650857117305 ], [ -76.49487712650371, 3.477856199867669 ], [ -76.495944627641109, 3.478840812671006 ], [ -76.496466302932049, 3.479254209490618 ], [ -76.48992146486772, 3.482897021885057 ] ] ] } },
+{ "type": "Feature", "properties": { "id": 3.0, "estado": "Bueno", "material": "Acero", "diametro": 14.0, "longitud": 2005.39546 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -76.484507426080739, 3.46559675480463 ], [ -76.48992146486772, 3.482897021885057 ] ] ] } },
+{ "type": "Feature", "properties": { "id": 4.0, "estado": "Regular", "material": "Acero", "diametro": 10.0, "longitud": 313.09034 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -76.494714972221999, 3.477650857117305 ], [ -76.495846409372163, 3.477303327398515 ], [ -76.497413632239642, 3.476836686582528 ] ] ] } },
+{ "type": "Feature", "properties": { "id": 5.0, "estado": "Regular", "material": "Acero", "diametro": 8.0, "longitud": 802.13232 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -76.49331794808748, 3.474628382458286 ], [ -76.491511814446767, 3.468594177278583 ], [ -76.490613850106428, 3.468899942830429 ] ] ] } }
+]
+}
